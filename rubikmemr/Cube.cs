0e5129e87c56623b7f128d8b5d8e2cd29fbef5af
Christian Roeer <croeer@users.noreply.github.com>
2022-01-17 08:39:21 +0100

    add more functions, play with imaging

--- rubikmemr/Cube.cs
+++ rubikmemr/Cube.cs
@@ -1,4 +1,6 @@
-﻿namespace rubikmemr
+﻿using System.Drawing;
+
+namespace rubikmemr
 {
 
     public enum Color
@@ -23,8 +25,6 @@
 
     public class Cube
     {
-        public const int CENTER = 4;
-
         public Color[,] State;
 
         public Cube(Color[,] state)
@@ -58,7 +58,6 @@
                   Color.white, Color.white, Color.white}
             };
         }
-
 
         #region "F moves"
 
@@ -576,7 +575,6 @@
         }
 
         #endregion
-
           
         #region "L moves"
 
@@ -681,6 +679,28 @@
 
         #endregion
 
+        #region "drawing'
 
+        public Image GetBitmap()
+        {
+            Image image = new Bitmap(2000, 1024);
+
+            Graphics graph = Graphics.FromImage(image);
+
+            graph.Clear(System.Drawing.Color.Azure);
+
+            Pen pen = new Pen(Brushes.Black);
+
+            graph.DrawLines(pen, new Point[] { new Point(10, 10), new Point(800, 900) });
+
+            Rectangle rect = new Rectangle(100, 100, 300, 300);
+            graph.DrawRectangle(pen, rect);
+
+            image.Save("myImage.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            return image;
+        }
+
+        #endregion
     }
 }
